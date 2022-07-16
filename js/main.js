73a39ed0001e517c99b02002092caf8a3e039047
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2022-07-16 15:19:11 +0100

    added javascrpt

--- js/main.js
+++ js/main.js
@@ -6,3 +6,34 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  new_li.innerText = getNumberOrString(document.getElementById('text').value);
+
+  element_list.appendChild(new_li);
+
+});
+
+
+function removelist() {
+
+  document.getElementById('button-remove').addEventListener('click', (event) => {
+    
+    let new_rli = document.getElementById('list');
+    new_rli.removeChild(new_rli.lastElementChild);
+}
+}
