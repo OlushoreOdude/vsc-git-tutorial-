84d95789c3218057bff10e9208e47a0a5d99cf20
OlushoreOdude <108965036+OlushoreOdude@users.noreply.github.com>
2022-07-23 10:23:53 +0100

    updated index html to add dynamic list

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ function myFunction() {
   }
 }
 
+function addList() {
+  
 function getNumberOrString(value) {
   // Convert a string value to a number if possible
   let number_value = Number(value);
@@ -19,26 +21,29 @@ function getNumberOrString(value) {
 
 
 
-document.getElementById('button').addEventListener('click', (event) => {
-  let element_list = document.getElementById('list');
+document.getElementById('button4').addEventListener('click', (event) => {
+  let element_list = document.getElementById('listpp');
   let new_li = document.createElement('li');
   new_li.innerText = getNumberOrString(document.getElementById('text').value);
 
   element_list.appendChild(new_li);
 
 });
-
+} 
 
 /*
-function removelist() {
 
-  let testrev = document.getElementById('list');
+function getList() {
+
+  let getListA = document.getElementById('list');
 }
-/*
-function removelist() {
 
+*/
+
+function removelist() {
   document.getElementById('button-remove').addEventListener('click', (event) => {
     
-    let new_rli = document.getElementById('list');
+    let new_rli = document.getElementById('listpp');
     new_rli.removeChild(new_rli.lastElementChild);
-});  */
+}); 
+}
